122cecac5e3a048ad8d7e7a479ea86b67b6bdecc
Rayyan <mhdrayyan86@gmail.com>
2025-05-18 14:38:54 +0530

    intro designed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./views/**/*.{html,hbs,js}",  // For Handlebars files
+    "./public/**/*.js"             // If you have JS using Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
